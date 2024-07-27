d685f0e1dc6aa4d53a5835d4a1b26c4b0f1f647e
vlad <vladshima99@gmail.com>
2024-07-27 23:41:36 +0300

    FuturePublishers

--- CombineApp/CombineApp/ViewController.swift
+++ CombineApp/CombineApp/ViewController.swift
@@ -8,20 +8,25 @@
 import UIKit
 import Combine
 
+enum CombineError: Error {
+    case failed
+}
+
 final class ViewController: UIViewController {
     
+    @IBOutlet weak var photoImageView: UIImageView!
     //Data
     let singleData = "Vlad"
     let data = ["One", "Two", "Three"]
     
     let viewModel = ViewModel()
     let just = JustPublishers()
+    let futurePublishers = FuturePublishers()
     
     var subscriptions = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
         
         // Publisher
         let simplePublisher = Just<String>(singleData)
@@ -54,6 +59,19 @@ final class ViewController: UIViewController {
         viewModel.requestSomething()
         just.subscribeInPublisher()
         just.subscribeData()
+        
+        futurePublishers.future.sink { completion in
+            switch completion {
+            case .finished:
+               print("Success Photo")
+            case .failure(let someError):
+                print("Error :\(someError)")
+            }
+        } receiveValue: { [weak self] image in
+            DispatchQueue.main.async {
+                self?.photoImageView.image = image
+            }
+        }.store(in: &subscriptions)
     }
 }
 
@@ -73,6 +91,26 @@ final class JustPublishers {
         data.publisher.sink { value in
             print("Sequence value: \(value)")
         }.store(in: &store)
+    }
+}
+
+final class FuturePublishers {
+    let future = Future<UIImage, CombineError> { promise in
+        let url = URL(string: "https://media.istockphoto.com/id/1441759606/ru/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D1%87%D0%B0%D1%81%D1%82%D0%BB%D0%B8%D0%B2%D0%B0%D1%8F-%D1%81%D0%BF%D0%BE%D1%80%D1%82%D1%81%D0%BC%D0%B5%D0%BD%D0%BA%D0%B0-%D1%81-%D0%BD%D0%B0%D1%83%D1%88%D0%BD%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B1%D0%B5%D0%B3%D0%B0%D0%B5%D1%82-%D0%B2-%D0%BF%D0%B0%D1%80%D0%BA%D0%B5.jpg?s=612x612&w=0&k=20&c=Apes_rqX1rj6DBiHKS8Ncn87b6cVv2RvzkfBtkUUwGE=")!
+        
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data , response , error in
+            guard error == nil else {
+                promise(.failure(.failed))
+                return
+            }
+            
+            guard let imageData = data else {
+                promise(.failure(.failed))
+                return
+            }
+            
+            promise(.success(UIImage(data: imageData)!))
+        }.resume()
     }
 }
 
